360ed756235a09ccd24f9b55bc60059528bbe540
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2023-05-03 08:59:45 +0200

    Done with the Model

--- script.ts
+++ script.ts
@@ -1,8 +1,18 @@
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
-
-
+async function  main() {
+// const user  =  await    prisma.user.create({data:{name:"innocent"}})
+const FindUser = await prisma.user.findMany()
+console.log(FindUser);
+}
+main()
+.catch((e) => {
+    console.error(e.message)
+})
+.finally(async () =>{
+    await prisma.$disconnect()
+})
 
 
 
